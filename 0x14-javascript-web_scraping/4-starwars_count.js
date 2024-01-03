64befafd76b918193d0ff39b8c37428e9189fa5d
Hafida Belayd <hafidabelaidagnaoui@gmail.com>
2024-01-03 10:15:04 -0800

    tASK

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
-// This script prints the number of movies where
-// the character “Wedge Antilles” is present.
-const axios = require('axios');
-const ID = '18';
-let total = 0;
 
-axios.get(process.argv[2])
-  .then(res => {
-    const films = res.data.results ? res.data.results : [];
-    const size = films.length;
-    for (let i = 0; i < size; i++) {
-      films[i].characters.forEach(chr => {
-        if (chr.includes(ID)) total++;
-      });
-    }
-    console.log(total);
-  })
-  .catch(err => {
-    console.log('Error:', err);
-  });
+const request = require('request');
+const characterId = '18';
+let characterUrl = 'https://swapi-api.alx-tools.com/api/people/';
+characterUrl += characterId + '/';
+const requestUrl = process.argv[2];
+
+request(requestUrl, (err, response, body) => {
+  if (err) {
+    throw (err);
+  }
+  const film = JSON.parse(body);
+  let noEpisodes = 0;
+  const results = film.results;
+  for (const result of results) {
+    const characters = result.characters;
+    const wedgeCharacter = characters.filter(n => n === characterUrl);
+    noEpisodes += wedgeCharacter.length;
+  }
+  console.log(noEpisodes);
+});
